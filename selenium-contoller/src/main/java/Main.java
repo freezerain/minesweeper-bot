be7a7402c3d122d13cfb62cad98599dd77edc509
Freeze <rain-ilia@rambler.ru>
2021-08-07 21:22:12 +0200

    First working bot

--- selenium-contoller/src/main/java/Main.java
+++ selenium-contoller/src/main/java/Main.java
@@ -1,25 +1,42 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        final String LOGIN = "freezerain";
+        final String PASS = "@Sj5cySbQWTPP!C";
         System.setProperty("webdriver.chrome.driver",
                            "D:\\Soft\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://minesweeper.online/ru/game/800460192");
-        sleep(2000);
-        WebElement selectMediumLevelButton = driver.findElement(By.id("level_select_2"));
-        selectMediumLevelButton.click();
-        sleep(2000);
-        driver.findElement(By.id("level_select_1")).click();
-        WebElement cell_1_0 = driver.findElement(By.id("cell_1_0"));
-        Actions actionProvider = new Actions(driver);
-        actionProvider.click(cell_1_0).build().perform();
-        
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("headless");*/
+        options.addArguments("--user-data-dir=D:\\Soft\\chromedriver_win32\\UserData");
+        WebDriver driver = new ChromeDriver(options);
+        //driver.manage().window().minimize();
+        driver.manage().window().setSize(new Dimension(1024, 800));
+        Scanner in = new Scanner(System.in);
+        SweeperSolver.startPlaying(driver, 2, 5, 500, 10);
+        /*while(true){
+            System.out.print("Difficulty(1-4) ?: ");
+            int difficulty = in.nextInt();
+            in.nextLine();
+            System.out.println("Games to play?: ");
+            int games = in.nextInt();
+            in.nextLine();
+            SweeperSolver.startPlaying(driver, difficulty, games, 3000, 5);
+            System.out.println("Gaming session end. Enter /again/ or /quit/ :");
+            String isRepeat = in.nextLine();
+            if(isRepeat.equals("again")) continue;
+            else break;
+        }*/
+        driver.quit();
     }
 }
